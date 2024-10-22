6eb84849cceafebf78187f17d9e0a39f4e3a7351
Sumit210106 <sumitnayak2006@gmail.com>
2024-10-22 21:37:56 +0530

    Update check_Palindrome.py
    i changed the code and made the code super short and user friendly with the help of python in built function and with the use string's functions of python... hope you like the changes

--- check_Palindrome.py
+++ check_Palindrome.py
@@ -1,30 +1,13 @@
-def isPalindrome(str,rev_str):
-    x = str == rev_str
-    return x
-
-while True:
-    try:
-        choice = int(input("Press the number corresponding to the option to select it\n0.\tExit\n1.\tCheck for Palindrome\n"))
-
-        if choice == 1:
-            string = input("Enter the string you want to check: ")
-            low_str = string.lower()
-            rev_str = low_str[-1::-1]
-
-            result = isPalindrome(low_str,rev_str)
-            if result == True:
-                print(f"\n{string} is a palindrome!")
-            elif result == False:
-                print(f"\n{string} is not a palindrome as it spells {rev_str} when reversed.")
-            else:
-                print("invalid input")
-
-        elif choice == 0:
-            print("\n####################\tThank you for using my program\t####################")
-            break
-        else:
-            print("\nInvalid input")
-
-    except:
-        print("\nEnter an integer value corresponding to your choice!")
-        
+def Palindrome(str_,rev_str):
+    str_=str(str_)
+    rev = str(str_[::-1])
+    if rev == rev_str:
+        print(f"{str_} and {rev_str} are palindrome of each other")
+    else:
+        print(f"{str_} and {rev_str} are not palindrome of each other")
+    return ""
+print("------Enter the string in the input section------")
+str_=input("Enter the frist string to check: ")
+rev_str=input("Enter the second string to check: ")
+print(Palindrome(str_,rev_str))
+print("THANKYOU FOR USING THE PROGRAM !!!")
